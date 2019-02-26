bf7994a4ac02ae47bbf8545c0851aa3efb564bcb
weifuxin <1126339699@qq.com>
2019-02-26 16:51:29 +0800

    modify replyWeChat

--- replyWeChat.py
+++ replyWeChat.py
@@ -9,8 +9,8 @@ itchat.auto_login(hotReload=True)
 #小冰的微信username
 MapName = '@40ac8c5b82141cb51f50d05b78f0ef75'
 #保存发送人微信UserName
-global  MyUserName
-MyUserName = ''
+global My_USER_NAME
+My_USER_NAME = ''
 
 #给公众号发送消息
 def send_gzh(str):
@@ -45,12 +45,12 @@ def get_reply_text(str):
 #添加isMpChat=True 接收公众号信息 	
 @itchat.msg_register(itchat.content.TEXT, isMpChat=True)
 def get_gzh_text(msg):
-    global MyUserName
+    global My_USER_NAME
     print('%s: %s' % (msg['Type'], msg['Text']), msg['FromUserName'])	
 	#return msg['Text']
-    #itchat.send(msg['Text'],toUserName = MyUserName)
-    print("保存的username"+ MyUserName)
-    itchat.send(msg['Text'], MyUserName)
+    #itchat.send(msg['Text'],toUserName = My_USER_NAME)
+    print("保存的username"+ My_USER_NAME)
+    itchat.send(msg['Text'], My_USER_NAME)
     #return msg['Text'] 如果return消息内容将会和公众号一直聊天
 
 #接收好友消息
@@ -70,8 +70,8 @@ def text_reply(msg):
 	send_gzh(str = msg.text)
 	#replyText = get_gzh_text(msg)
 	print("接收到的消息:replyText" + replyText)
-	global MyUserName
-	MyUserName = msg['FromUserName']
+	global My_USER_NAME
+	My_USER_NAME = msg['FromUserName']
 	msg.user.send("小新智能机器人自动回复:\n 上班时间将由智能机器人自动给您回复，如有急事请及时电话联系！")
 	print("智能回复消息:" + msg.text)
 	print("---------------------------------------------")
@@ -87,7 +87,7 @@ python 微信智能聊天机器人流程
 	a.接收发送人消息、username等内容，保存username到全局变量
 	b.吧接收到的内容发送给小冰
 	c.接收小冰返回的消息，转发到好友
-	注意:普通开发者足够使用，未做压力测试，消息太多可能会串发消息，需要修改把MyUserName修改成队列
+	注意:普通开发者足够使用，未做压力测试，消息太多可能会串发消息，需要修改把My_USER_NAME修改成队列
 	
 	方案二:
 	a.接收发送人消息、username等内容，
